b36fd98b6043d468a879cb070b6832581286da1f
Yebin <puding2564@gmail.com>
2025-01-20 05:04:08 +0000

    Add aug_method arg

--- simclr/modules/transformations/simclr.py
+++ simclr/modules/transformations/simclr.py
@@ -1,5 +1,9 @@
+import torch
 import torchvision
-
+import torchvision.transforms.functional as F
+import random
+from PIL import ImageFilter
+import numpy as np
 
 class TransformsSimCLR:
     """
@@ -8,20 +12,100 @@ class TransformsSimCLR:
     denoted x ̃i and x ̃j, which we consider as a positive pair.
     """
 
-    def __init__(self, size):
+    def __init__(self, size, method):
         s = 1
         color_jitter = torchvision.transforms.ColorJitter(
             0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s
         )
-        self.train_transform = torchvision.transforms.Compose(
-            [
-                torchvision.transforms.RandomResizedCrop(size=size),
-                torchvision.transforms.RandomHorizontalFlip(),  # with 0.5 probability
-                torchvision.transforms.RandomApply([color_jitter], p=0.8),
-                torchvision.transforms.RandomGrayscale(p=0.2),
-                torchvision.transforms.ToTensor(),
-            ]
-        )
+        
+        if method == "crop":
+            self.train_transform = torchvision.transforms.Compose(
+                [
+                    torchvision.transforms.RandomResizedCrop(size=size),
+                    torchvision.transforms.ToTensor(),
+                ]
+            )
+        elif method == "flip":
+            self.train_transform = torchvision.transforms.Compose(
+                [
+                    torchvision.transforms.RandomHorizontalFlip(),
+                    torchvision.transforms.ToTensor(),
+                ]
+            )
+        elif method == "color_jitter":
+            self.train_transform = torchvision.transforms.Compose(
+                [
+                    torchvision.transforms.RandomApply([color_jitter], p=0.8),
+                    torchvision.transforms.ToTensor(),
+                ]
+            )
+        elif method == "grayscale":
+            self.train_transform = torchvision.transforms.Compose(
+                [
+                    torchvision.transforms.RandomGrayscale(p=0.2),
+                    torchvision.transforms.ToTensor(),
+                ]
+            )
+        elif method == "translation":
+            self.train_transform = torchvision.transforms.Compose(
+                [
+                    torchvision.transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)),
+                    torchvision.transforms.ToTensor(),
+                ]
+            )
+        elif method == "shearing":
+            self.train_transform = torchvision.transforms.Compose(
+                [
+                    torchvision.transforms.RandomAffine(degrees=0, shear=10),
+                    torchvision.transforms.ToTensor(),
+                ]
+            )
+        elif method == "rotation":
+            self.train_transform = torchvision.transforms.Compose(
+                [
+                    torchvision.transforms.RandomRotation(degrees=30),
+                    torchvision.transforms.ToTensor(),
+                ]
+            )
+        elif method == "cutout":
+            self.train_transform = torchvision.transforms.Compose(
+                [
+                    torchvision.transforms.RandomErasing(p=0.5, scale=(0.02, 0.33), ratio=(0.3, 3.3)),
+                    torchvision.transforms.ToTensor(),
+                ]
+            )
+        elif method == "noise_injection":
+            self.train_transform = torchvision.transforms.Compose(
+                [
+                    torchvision.transforms.Lambda(lambda img: img + 0.1 * torch.randn_like(img)),
+                    torchvision.transforms.ToTensor(),
+                ]
+            )
+        elif method == "kernel_filtering":
+            self.train_transform = torchvision.transforms.Compose(
+                [
+                    torchvision.transforms.Lambda(lambda img: img.filter(ImageFilter.GaussianBlur(radius=2))),
+                    torchvision.transforms.ToTensor(),
+                ]
+            )
+        elif method == "random_erasing":
+            self.train_transform = torchvision.transforms.Compose(
+                [
+                    torchvision.transforms.RandomErasing(p=0.5),
+                    torchvision.transforms.ToTensor(),
+                ]
+            )
+        else:
+            # original code
+            self.train_transform = torchvision.transforms.Compose(
+                [
+                    torchvision.transforms.RandomResizedCrop(size=size),
+                    torchvision.transforms.RandomHorizontalFlip(),
+                    torchvision.transforms.RandomApply([color_jitter], p=0.8),
+                    torchvision.transforms.RandomGrayscale(p=0.2),
+                    torchvision.transforms.ToTensor(),
+                ]
+            )
 
         self.test_transform = torchvision.transforms.Compose(
             [
